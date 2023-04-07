bc5a2e1432f24ddd5616f39b2a17973e10cd5949
pl1me12 <pl1me12@mail.ru>
2023-04-07 11:47:31 +0300

    1

--- main.py
+++ main.py
@@ -1,31 +1,43 @@
 import nltk
-nltk.download('punkt') # загрузка необходимых данных для NLTK
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from nltk import pos_tag, ne_chunk
+nltk.download('punkt')
 
 filename = input("Введите имя файла: ")
 word = input("Введите слово: ")
+sentences = []  # список для хранения предложений с искомым словом
 
-with open(filename, "r") as file:
-    text = file.read()
+# открываем файл и считываем все содержимое
+with open(filename, "r", encoding='utf-8') as f:
+    text = f.read()
 
-sentences = nltk.sent_tokenize(text)
+# токенизируем текст на предложения и слова
+tokenized_text = sent_tokenize(text)
+words = word_tokenize(text)
 
-for sentence in sentences:
-    words = nltk.word_tokenize(sentence)
-    if word in words:
-        tags = nltk.pos_tag(words)
-        for i, (w, tag) in enumerate(tags):
-            if w == word:
-                print("Слово:", w)
-                print("Предложение:", sentence)
-                print("Часть речи:", tag)
-                if tag in ['NN', 'NNS', 'NNP', 'NNPS']: # определение рода для существительных
-                    word_lemmatizer = nltk.WordNetLemmatizer()
-                    lemma = word_lemmatizer.lemmatize(w) # лемматизация слова
-                    noun_synset = nltk.corpus.wordnet.synsets(lemma, pos='n') # поиск синсетов существительных в WordNet
-                    if noun_synset: # если синсет найден, печатаем его информацию
-                        noun = noun_synset[0]
-                        print("Род:", noun.lexname().split('.')[0])
-                tree = nltk.parse.parse_one(sentence) # получение синтаксического дерева
-                print("Синтаксический разбор:")
-                tree.pretty_print() # вывод синтаксического дерева
-                print("="*80) # разделитель между результатами
+# определяем стоп-слова
+stop_words = set(stopwords.words("english"))
+
+# проходимся по каждому предложению
+for sentence in tokenized_text:
+    # токенизируем предложение на слова
+    sentence_words = word_tokenize(sentence)
+    # проверяем, есть ли искомое слово в предложении
+    if word in sentence_words:
+        # убираем стоп-слова из предложения
+        filtered_words = [word for word in sentence_words if word.casefold() not in stop_words]
+        # определяем части речи слов в предложении
+        tagged_words = pos_tag(filtered_words)
+        # определяем синтаксический разбор предложения
+        parsed_sentence = ne_chunk(tagged_words)
+        # добавляем предложение в список, если оно содержит искомое слово
+        sentences.append((sentence, parsed_sentence))
+
+# выводим результаты
+if len(sentences) > 0:
+    for sentence, parsed_sentence in sentences:
+        print("Найдено в предложении: ", sentence)
+        print("Части речи слов: ", parsed_sentence)
+else:
+    print("Искомое слово не найдено в тексте.")
